08b0654b67d2694bd4776621b5a06301115e8d83
Enrique Andrade Vale <56207998+Baam7@users.noreply.github.com>
2022-04-07 19:50:47 -0300

    Add files via upload
    Código utilizado para iniciar a stream da camêra do tello.
    Objetiva-se por meio do código, evitar erros devido ao atraso do ínicio da stream que pode causar problemas durante o primeiro voo de um sistema
    Portanto, antes de voar com o Tello pela primeira vez após ligar o computador, executa-se este código

--- scripts/streamon_inicial.py
+++ scripts/streamon_inicial.py
@@ -1,3 +1,9 @@
+''' Código utilizado para iniciar a stream da camêra do tello.
+Objetiva-se por meio do código, evitar erros devido ao atraso do ínicio da stream
+que pode causar problemas durante o primeiro voo de um sistema
+    Portanto, antes de voar com o Tello pela primeira vez após ligar o computador,
+executa-se este código'''
+
 import cv2
 from djitellopy import Tello
 
